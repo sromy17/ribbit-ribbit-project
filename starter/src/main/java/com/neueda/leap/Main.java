91c9cdec4ef24ea325d33f91a9424e54e8913c55
EC2 Default User <ec2-user@ip-10-18-66-21.eu-west-1.compute.internal>
2026-08-19 19:59:30 +0000

    bring dockerSkeleton to dev

--- starter/src/main/java/com/neueda/leap/Main.java
+++ starter/src/main/java/com/neueda/leap/Main.java
@@ -2,7 +2,6 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO: replace <team-name> with your team's actual name
-        System.out.println("Hello world from the firm's Sprint 1 project skeleton");
+        System.out.println("Hello world from team Ribbit Ribbit(funny) skeleton");
     }
 }
